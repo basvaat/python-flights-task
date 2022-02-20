085453edf643b48554222d8dceb6feadc41dff0d
Szilvi <basvaat@gmail.com>
2022-02-20 12:27:55 +0100

    delete unused funcion

--- utils/reader.py
+++ utils/reader.py
@@ -4,21 +4,17 @@ class Reader:
     def __init__(self, inputPath):
         self.inputPath = inputPath
 
-    def csv(self):
-        with open(self.inputPath, newline='') as f:
-            csvReader = csv.reader(f)
-            w = []
-            for row in csvReader:
-                w.extend(row)
-        return w
 
     def csv_as_list_of_dicts(self):
         data = {}
+        try:
+            with open(self.inputPath) as csvFile:
+                csvReader = csv.DictReader(csvFile)
+                id = 0
+                for row in csvReader:
+                    data[id] = dict(row)
+                    id += 1
+            return data
+        except:
+            raise FileNotFoundError('File not found. File name or path is incorrect.')
         
-        with open(self.inputPath) as csvFile:
-            csvReader = csv.DictReader(csvFile)
-            id = 0
-            for row in csvReader:
-                data[id] = dict(row)
-                id += 1
-        return data
